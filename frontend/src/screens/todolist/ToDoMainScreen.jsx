5809b8a4ebcb795bd2b7e2035974b0b5d2096641
Haneesh Raj <haneeshraj.banisetti@gmail.com>
2022-03-10 12:01:09 +0530

    Bug fixes Login and Registration

---
+++ frontend/src/screens/todolist/ToDoMainScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ToDoMainScreen.css";
+
+const ToDoMainScreen = () => {
+  return (
+    <>
+      <h1 id='tsms-heading'>To-Dos</h1>
+    </>
+  );
+};
+
+export default ToDoMainScreen;
